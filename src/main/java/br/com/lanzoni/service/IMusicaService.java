ad1028bd7faf575e7a54ba973adc1bf48cbd2396
Vinicius Bruno Lanzoni <viniciuslanzoni@tokenlab.com.br>
2019-06-20 12:21:42 -0300

    MusicaService e a camada de serviço

--- src/main/java/br/com/lanzoni/service/IMusicaService.java
+++ src/main/java/br/com/lanzoni/service/IMusicaService.java
@@ -1,4 +1,15 @@
 package br.com.lanzoni.service;
 
+import br.com.lanzoni.domain.Musica;
+
+import java.util.List;
+
 public interface IMusicaService {
+
+    void salvar(Musica musica, long playlistId);
+    List<Musica> recuperarPorPlaylistId(long playlistId);
+    Musica recuperarPorPlaylistIdEMusicaId(long playlistId, long musicaId);
+    void atualizar(Musica musica, long playlistId);
+    void excluir(long playlistId, long musicaId);
+
 }
---
+++ src/main/java/br/com/lanzoni/service/MusicaService.java
@@ -0,0 +1,49 @@
+package br.com.lanzoni.service;
+
+import br.com.lanzoni.dao.IMusicaDao;
+import br.com.lanzoni.domain.Musica;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class MusicaService implements IMusicaService{
+
+    @Autowired
+    private IMusicaDao musicaDao;
+
+    @Autowired
+    private IPlaylistService playlistService;
+
+    @Override
+    public void salvar(Musica musica, long playlistId) {
+        musica.setPlaylist(playlistService.recuperarPorId(playlistId));
+        musicaDao.salvar(musica);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Musica> recuperarPorPlaylistId(long playlistId) {
+        return musicaDao.recuperarPorPlaylist(playlistId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Musica recuperarPorPlaylistIdEMusicaId(long playlistId, long musicaId) {
+        return musicaDao.recuperarPorPlaylistEMusica(playlistId, musicaId);
+    }
+
+    @Override
+    public void atualizar(Musica musica, long playlistId) {
+        musica.setPlaylist(playlistService.recuperarPorId(playlistId));
+        musicaDao.atualizar(musica);
+    }
+
+    @Override
+    public void excluir(long playlistId, long musicaId) {
+        musicaDao.excluir(recuperarPorPlaylistIdEMusicaId(playlistId, musicaId).getId());
+    }
+}
