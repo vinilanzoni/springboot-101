279d4f816a6907082ef4ef6a7637db84d0867981
Vinicius Bruno Lanzoni <viniciuslanzoni@tokenlab.com.br>
2019-06-20 12:40:47 -0300

    Executando o projeto Spring Boot

---
+++ src/main/java/br/com/lanzoni/SpotMusicApplication.java
@@ -0,0 +1,13 @@
+package br.com.lanzoni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotMusicApplication.class, args);
+    }
+
+}
